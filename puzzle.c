c4d78f1cb82e8ee6299c0b9d5c73070c10e28d4b
roelleal <vegerex91@gmail.com>
2024-04-11 22:31:41 -0600

    PUZZLE TERMINADO NUEVO

--- puzzle.c
+++ puzzle.c
@@ -4,7 +4,7 @@
 #define flecha_der 77 // FLECHA DERECHA
 #define flecha_izq 75 // FLECHA IZQUIERDA
 #define esc 27 // ESC SALIDA
-int posI = 3, posJ = 1, temp = 0;
+int posI = 3, posJ = 1, temp = 0, n = 0;
 int puzzle[4][4] = 
 	{
 		{1, 5, 4, 8},
@@ -60,30 +60,29 @@ void moverDerecha() {
 }
 int main() {
 	char flecha;
-	int i = 0, j = 0;
+	int i = 0, j = 0, der = 0, izq = 0, aba = 0, arr = 0;
 	imprimirArreglo();
 	while(1) {
-		flecha = getch();
-		switch(flecha) {
-			case flecha_arr:
-				moverArriba();
-				imprimirArreglo();
-				break;
-			case flecha_aba:
-				moverAbajo();
-				imprimirArreglo();
-				break;
-			case flecha_der:
-				moverDerecha();
-				imprimirArreglo();
-				break;
-			case flecha_izq:
-				moverIzquierda();
-				imprimirArreglo();
-				break;
-			case esc:
-				return 0;
-				break;
+		printf("Ingrese el numero que desea mover\n");
+		scanf("%i", &n);
+		der = puzzle[posI][posJ+1];
+		izq = puzzle[posI][posJ-1];
+		aba = puzzle[posI+1][posJ];
+		arr = puzzle[posI-1][posJ];
+		if(n == der) {
+			moverDerecha();
+			imprimirArreglo();
+		}else if(n == izq) {
+			moverIzquierda();
+			imprimirArreglo();
+		}else if(n == aba) {
+			moverAbajo();
+			imprimirArreglo();
+		}else if(n == arr) {
+			moverArriba();
+			imprimirArreglo();
+		}else {
+			printf("El numero %i no se puede ser seleccionado, elija otro numero\n\n", n);
 		}
 	}
 }
